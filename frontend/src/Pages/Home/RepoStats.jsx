a9755db56fd96b78994fdea4d1be9b7336c7a543
Shreyashpawar28 <shreyashpawar0000@gmail.com>
2024-02-28 01:09:18 +0530

    Responsive homepage for mobile Issue #13

--- frontend/src/Pages/Home/RepoStats.jsx
+++ frontend/src/Pages/Home/RepoStats.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Box, Heading, Text, Spinner, Flex } from '@chakra-ui/react';
+import { Box, Heading, Text, Spinner, Flex, Wrap, WrapItem, Center } from '@chakra-ui/react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar, faEye, faCodeBranch, faExclamationCircle } from '@fortawesome/free-solid-svg-icons';
 
@@ -34,24 +34,24 @@ const GitHubRepoStats = () => {
   return (
     <Box p="4" bg='#0A1B1E' color='white'>
       <Heading textAlign="center" fontSize="2.5rem" pb="2rem">GitHub Repository</Heading>
-      <Flex justifyContent='space-between' alignItems="center" padding='0px 300px'>
-        <Flex alignItems="center">
+      <Wrap spacing='30px' justify='center' padding={{lg:'0px 300px',base:'0px 40px'}}>
+        <WrapItem alignItems="center" padding={{lg:'0px 50px'}}>
           <FontAwesomeIcon color='yellow' icon={faStar} style={{ marginRight: '0.5rem' }} />
           <Text color='yellow'>Stars: {repoStats.stargazers_count}</Text>
-        </Flex>
-        <Flex alignItems="center">
+        </WrapItem>
+        <WrapItem alignItems="center" padding={{lg:'0px 50px'}}>
           <FontAwesomeIcon color='white' icon={faEye} style={{ marginRight: '0.5rem' }} />
           <Text color='white'>Watchers: {repoStats.watchers_count}</Text>
-        </Flex>
-        <Flex alignItems="center">
+        </WrapItem>
+        <WrapItem alignItems="center" padding={{lg:'0px 50px'}}>
           <FontAwesomeIcon color='green' icon={faCodeBranch} style={{ marginRight: '0.5rem' }} />
           <Text color='green'>Forks: {repoStats.forks_count}</Text>
-        </Flex>
-        <Flex alignItems="center">
+        </WrapItem>
+        <WrapItem alignItems="center" padding={{lg:'0px 50px'}}>
           <FontAwesomeIcon color='red' icon={faExclamationCircle} style={{ marginRight: '0.5rem' }} />
           <Text color='red'>Open Issues: {repoStats.open_issues_count}</Text>
-        </Flex>
-      </Flex>
+        </WrapItem>
+      </Wrap>
     </Box>
   );
 };
